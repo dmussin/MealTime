d47b59ac5e8e2e832e660f39e2d3a5683f3d1d4c
Daniyar Mussin <45901913+stormy377@users.noreply.github.com>
2021-06-03 13:17:57 +0200

    Initial Commit

---
+++ MealTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealTime
+//
+//  Created by Daniyar Mussin on 03.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
