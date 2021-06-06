34fb0e1b968ff024e95fbad93d755e4a12aef7a8
Daniyar Mussin <45901913+stormy377@users.noreply.github.com>
2021-06-06 11:27:08 +0200

    Creating Model and Entites

---
+++ CoreData Entities/Meal+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Daniyar Mussin on 06.06.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
